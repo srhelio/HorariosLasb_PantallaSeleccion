dcf359155964360774b58e8400b02e85c1c231ab
ManuelV <srhelio@gmail.com>
2025-03-09 09:37:38 +0100

    Se quitan los comentarios iniciales

--- SelectorHorarios.py
+++ SelectorHorarios.py
@@ -1,14 +1,3 @@
-'''
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix import button
-
-class kivySaludo(App):
-    def build(self):
-        return Label(text="Buenos dias")
-
-'''
-
 from tkinter import *
 import webbrowser
 
@@ -292,31 +281,6 @@ buttonLibre = Button(root, text = 'LIBRES',
                  font=('arial', 18, 'bold'),
                  command=openLabLibre).grid(row=3, column=9, padx = 20)
 
-
-
-#entry = ttk.Entry(root)
-
-# define a grid
-'''
-window.columnconfigure((0,1,2), weight = 1, uniform = 'a')
-
-window.rowconfigure(0, weight = 1, uniform = 'a')
-window.rowconfigure(1, weight = 1, uniform = 'a')
-window.rowconfigure(2, weight = 1, uniform = 'a')
-window.rowconfigure(3, weight = 3, uniform = 'a')
-'''
-# place a widget
-#label1.grid(row = 0, column = 0, sticky = 'nsew')
-'''
-label2.grid(row = 1, column = 1, rowspan = 3, sticky = 'nsew')
-label3.grid(row = 1, column = 0, columnspan = 3, sticky = 'nsew', padx = 20, pady = 10)
-label4.grid(row = 3, column = 3, sticky = 'se')
-'''
-# exercise 
-# add the buttons and the entry field
-#button1.grid(row = 0, column = 3, sticky = 'nesw')
-#button2.grid(row = 2, column = 2, sticky = 'nesw')
-#entry.grid(row = 2, column = 10, rowspan = 2, sticky = 'ew')
 
 
 root.mainloop()
